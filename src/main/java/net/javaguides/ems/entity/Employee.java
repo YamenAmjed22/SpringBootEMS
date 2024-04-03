2f0aadc131ed971c7d46f679a3ba1e61c9668110
Yamen Amjed <yamenamjed2022@gmail.com>
2024-04-03 12:23:48 -0700

    Initial commit

---
+++ src/main/java/net/javaguides/ems/entity/Employee.java
@@ -0,0 +1,64 @@
+package net.javaguides.ems.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    //ATT
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id ;
+    @Column(name = "first_name")
+    private String firstName ;
+    @Column(name = "last_name")
+    private String lastName ;
+    @Column(name = "email_id" , nullable = false , unique = true)
+    private String email ;
+    //constructor in two type
+    //all parameters
+    public Employee(long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    //no parameters
+
+    public Employee() {
+    }
+
+    //Getter and Setter
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
